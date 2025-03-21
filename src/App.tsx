908f49ceddcc0021cd38dbda53bd3af591c51e04
Ravichandran Jayaramegowda <rjayaramegowda@gmail.com>
2025-03-21 20:06:29 +0400

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import Dashboard from "./pages/Dashboard";
-import { FilterList } from "./pages/FilterList";
-import ProfileDetailsPage from "./pages/ProfileDetailsPage";
-import ProfileListPage from "./pages/ProfileListPage";
-import UserListCol from "./pages/UserListCol";
+import React from "react";
+import AppMain from "./AppMain";
 import { useProfilesQuery } from "./reducers/api/profilesApi";
 
 const App = () => {
   const { data, error, isLoading, isFetching, isSuccess } = useProfilesQuery();
 
-  return (
-    <main className="row">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<ProfileListPage />} />
-          <Route path="details" element={<ProfileDetailsPage />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          {/* <Route element={<AuthLayout />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-
-          <Route path="concerts">
-            <Route index element={<ConcertsHome />} />
-            <Route path=":city" element={<City />} />
-            <Route path="trending" element={<Trending />} />
-          </Route> */}
-        </Routes>
-      </BrowserRouter>
-    </main>
-  );
+  const showView = isSuccess ? <AppMain /> : <h1>Loading</h1>;
+  return <>{showView}</>;
 };
 
 export default App;
---
+++ src/AppMain.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import ProfileDetailsPage from "./pages/ProfileDetailsPage";
+import ProfileListPage from "./pages/ProfileListPage";
+
+const AppMain = () => {
+  return (
+    <main className="row">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<ProfileListPage />} />
+          <Route path="details" element={<ProfileDetailsPage />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* <Route element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+
+          <Route path="concerts">
+            <Route index element={<ConcertsHome />} />
+            <Route path=":city" element={<City />} />
+            <Route path="trending" element={<Trending />} />
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+};
+
+export default AppMain;
