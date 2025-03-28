dc2c7cffe72fa35fb36414b38941a3fb34694111
Ravichandran Jayaramegowda <rjayaramegowda@gmail.com>
2025-03-28 23:24:58 +0400

    added auth pages

---
+++ src/models/authuser.model.ts
@@ -0,0 +1,28 @@
+export interface Authuser {
+    uid: string
+    email: string
+    emailVerified: boolean
+    isAnonymous: boolean
+    providerData: ProviderDaum[]
+    stsTokenManager: StsTokenManager
+    createdAt: string
+    lastLoginAt: string
+    apiKey: string
+    appName: string
+  }
+  
+  export interface ProviderDaum {
+    providerId: string
+    uid: string
+    displayName: any
+    email: string
+    phoneNumber: any
+    photoURL: any
+  }
+  
+  export interface StsTokenManager {
+    refreshToken: string
+    accessToken: string
+    expirationTime: number
+  }
+  
